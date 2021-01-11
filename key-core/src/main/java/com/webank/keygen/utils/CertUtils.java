c8d5bf269d03625660410cb112c45a726c42c53b
卡拉马佐夫 <973459080@qq.com>
2021-01-11 17:08:12 +0800

    clean cert code (#14)
    clean cert code

--- key-core/src/main/java/com/webank/keygen/utils/CertUtils.java
+++ key-core/src/main/java/com/webank/keygen/utils/CertUtils.java
@@ -18,30 +18,30 @@ package com.webank.keygen.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
 import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.*;
-import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.AuthorityKeyIdentifier;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509ExtensionUtils;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.DigestCalculator;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.bc.BcDigestCalculatorProvider;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import org.bouncycastle.pkcs.PKCS10CertificationRequest;
-import org.bouncycastle.util.io.pem.PemReader;
 
-import java.io.*;
 import java.math.BigInteger;
-import java.security.*;
-import java.security.cert.CertificateException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
 import java.security.cert.X509Certificate;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
@@ -110,142 +110,5 @@ public class CertUtils {
 				new BcDigestCalculatorProvider().get(new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1));
 
 		return new X509ExtensionUtils(digCalc).createAuthorityKeyIdentifier(publicKeyInfo);
-	}
-
-	public static PEMKeyPair readKey(String filePath) throws FileNotFoundException {
-		Object object = readPEMObject(filePath);
-		if (object instanceof PEMKeyPair) {
-			return (PEMKeyPair) object;
-		}
-		return null;
-	}
-
-	public static Key readRSAKey(String filePath) throws Exception {
-		Object object = readPEMObject(filePath);
-		if (object instanceof PEMKeyPair) {
-			return KeyFactory.getInstance("RSA").generatePrivate(
-					new PKCS8EncodedKeySpec(((PEMKeyPair)object).getPrivateKeyInfo().getEncoded()));
-		}
-		return null;
-	}
-
-	public static void writeKey(Key key, String filePath) {
-		writeToFile(key, filePath);
-	}
-
-	public static X509Certificate readCrt(String filePath) throws CertificateException, FileNotFoundException {
-		Object object = readPEMObject(filePath);
-		if (object instanceof X509CertificateHolder) {
-			return new JcaX509CertificateConverter().setProvider("BC")
-					.getCertificate((X509CertificateHolder) object);
-		}
-		return null;
-	}
-
-	public static X509Certificate convertStrToCert(String crtStr) throws CertificateException {
-		Object object = readStringAsPEM(crtStr);
-		if (object instanceof X509CertificateHolder) {
-			return new JcaX509CertificateConverter().setProvider("BC")
-					.getCertificate((X509CertificateHolder) object);
-		}
-		return null;
-	}
-
-	public static PKCS10CertificationRequest convertStrToCsr(String csrStr) {
-		Object object = readStringAsPEM(csrStr);
-		if (object instanceof PKCS10CertificationRequest) {
-			return (PKCS10CertificationRequest) object;
-		}
-		return null;
-	}
-
-	public static void writeCrt(X509Certificate certificate, String filePath) throws FileNotFoundException {
-		writeToFile(certificate, filePath);
-	}
-
-	public static PKCS10CertificationRequest readCsr(String filePath) throws FileNotFoundException {
-		Object object = readPEMObject(filePath);
-		if (object instanceof PKCS10CertificationRequest) {
-			return (PKCS10CertificationRequest) object;
-		}
-		return null;
-	}
-
-	public static void writeCsr(PKCS10CertificationRequest request, String filePath) throws FileNotFoundException {
-		writeToFile(request, filePath);
-	}
-
-	public static void writeToFile(Object object, String filePath) {
-		try (JcaPEMWriter pw = new JcaPEMWriter(new FileWriter(filePath))) {
-			pw.writeObject(object);
-		} catch (IOException e) {
-			log.error("writeObject failed", e);
-		}
-	}
-
-	public static Object readPEMObject(String filePath) throws FileNotFoundException {
-		if (!FileOperationUtils.exist(filePath)){
-			throw new FileNotFoundException("filePath does't exist，path = " + filePath);
-		}
-		PemReader pemReader = null;
-		Object object = null;
-		try {
-			pemReader = new PemReader(new FileReader(filePath));
-			PEMParser pemParser = new PEMParser(pemReader);
-			object = pemParser.readObject();
-		} catch (IOException e) {
-			log.error("readPEMObject failed", e);
-		} finally {
-			if (pemReader != null) {
-				try {
-					pemReader.close();
-				} catch (IOException e) {
-					log.error("pemReader.close failed", e);
-				}
-			}
-		}
-		return object;
-	}
-
-	public static String readPEMAsString(Object object){
-		String result = null;
-		StringWriter writer = new StringWriter();
-		JcaPEMWriter pw = new JcaPEMWriter(writer);
-		try {
-			pw.writeObject(object);
-		} catch (IOException e) {
-			log.error("pw.writeObject failed ",e);
-		}finally {
-			try {
-				writer.close();
-				pw.close();
-			} catch (IOException e) {
-				log.error("io close failed", e);
-			}
-		}
-		if (writer.getBuffer() != null) {
-			result = writer.getBuffer().toString();
-		}
-		return result;
-	}
-
-	public static Object readStringAsPEM(String string){
-		StringReader reader = new StringReader(string);
-		PemReader pemReader = new PemReader(reader);
-		PEMParser pemParser = new PEMParser(pemReader);
-		Object object = null;
-		try {
-			object = pemParser.readObject();
-		} catch (IOException e) {
-			log.error("readPEMObject failed", e);
-		} finally {
-			try {
-				reader.close();
-				pemReader.close();
-			} catch (IOException e) {
-				log.error("pemReader.close failed", e);
-			}
-		}
-		return object;
 	}
 }
