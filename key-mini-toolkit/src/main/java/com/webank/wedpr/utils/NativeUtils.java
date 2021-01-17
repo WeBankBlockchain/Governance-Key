f0f1a751764f4896692011668d15655d3605f534
cyjseagull <yujiechen_hust@163.com>
2021-01-17 13:56:52 +0800

    add java.library.ffipath property to support load ffi from the given path

--- key-mini-toolkit/src/main/java/com/webank/wedpr/utils/NativeUtils.java
+++ key-mini-toolkit/src/main/java/com/webank/wedpr/utils/NativeUtils.java
@@ -1,7 +1,5 @@
 package com.webank.wedpr.utils;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,18 +10,38 @@ import java.nio.file.*;
 
 /**
  * load native resource
- * @author aaronchu
- * @Description
+ *
+ * @author aaronchu @Description
  * @data 2020/06/18
  */
 public class NativeUtils {
 
-    public static void loadLibrary(String resourcePath) throws IOException{
+    public static void loadLibrary(String resourcePath) throws IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         loadLibrary(resourcePath, classLoader);
     }
 
-    public static void loadLibrary(String resourcePath, ClassLoader classLoader) throws IOException {
+    public static boolean loadLibraryFromLibraryPath(String libraryDirPath, String resourcePath)
+            throws IOException {
+        String fileName = deduceFileName(resourcePath);
+        File libraryFilePath = new File(new File(libraryDirPath), fileName);
+        if (!libraryFilePath.exists() || !new File(libraryDirPath).exists()) {
+            return false;
+        }
+        System.load(libraryFilePath.getAbsolutePath());
+        return true;
+    }
+
+    public static void loadLibrary(String resourcePath, ClassLoader classLoader)
+            throws IOException {
+        String libraryDirPath = System.getProperty("java.library.ffipath");
+        // load the library from the libraryPath
+        if (libraryDirPath != null && !libraryDirPath.isEmpty()) {
+            if (loadLibraryFromLibraryPath(libraryDirPath, resourcePath)) {
+                return;
+            }
+        }
+
         File tmpDir = new File(System.getProperty("user.home"));
         if (!tmpDir.exists() || !tmpDir.isDirectory()) {
             throw new IOException("user dir unavailable");
@@ -39,41 +57,45 @@ public class NativeUtils {
         String fileName = deduceFileName(resourcePath);
         File tmpFile = new File(ffiDir, fileName);
 
-        //To make sure write and load is atomic, incase p1 writes fails p2 load
+        // To make sure write and load is atomic, incase p1 writes fails p2 load
         File lockFile = new File(ffiDir, "native.lock");
         lockFile.deleteOnExit();
         FileLock lock = null;
         try (FileChannel c = new FileOutputStream(lockFile, true).getChannel()) {
             lock = c.lock();
 
-            try (InputStream input = classLoader.getResourceAsStream(resourcePath);) {
+            try (InputStream input = classLoader.getResourceAsStream(resourcePath); ) {
                 if (input == null) {
-                    throw new IOException("Resource not found:" + resourcePath + " for classloader " + classLoader.toString());
+                    throw new IOException(
+                            "Resource not found:"
+                                    + resourcePath
+                                    + " for classloader "
+                                    + classLoader.toString());
                 }
                 Files.copy(input, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             } catch (AccessDeniedException e) {
-                //In case that p1 load fails p2 write
+                // In case that p1 load fails p2 write
             }
 
             System.load(tmpFile.getAbsolutePath());
-        }
-        finally {
+        } finally {
             if (lock != null) {
-                try{
+                try {
                     lock.release();
                     lockFile.delete();
-                }catch (Exception e){}
+                } catch (Exception e) {
+                }
             }
         }
     }
 
-    private static String deduceFileName(String path){
+    private static String deduceFileName(String path) {
         String[] parts = path.split("/");
-        if(parts.length > 0){
-            return parts[parts.length-1];
+        if (parts.length > 0) {
+            return parts[parts.length - 1];
         }
-        throw new IllegalArgumentException("invalid path "+path);
+        throw new IllegalArgumentException("invalid path " + path);
     }
 
-    private NativeUtils(){}
+    private NativeUtils() {}
 }
