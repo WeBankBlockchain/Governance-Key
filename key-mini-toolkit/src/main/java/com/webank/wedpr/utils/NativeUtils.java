9fc0a3a5fcd58af80d5ab6bb5c75c4b87e86fd25
Aaron Y Chu <aoronchu@gmail.com>
2021-01-19 16:50:51 +0800

    Mini toolkit (#19)
    * update doc
    
    * update docs
    
    * update doc
    
    * .
    
    * 无配置即可体验
    
    * 重新组织下config宝
    
    * 添加mini-toolkit
    
    * no more delete on exist
    
    * fix typo
    
    * update interface
    
    * update path
    
    * support ofr arm
    
    * fix ql
    
    * Add files via upload
    
    * 进程安全性
    
    * upload snapshot version
    
    * fix typo
    
    * remove print
    
    * modify version to 1.0.2
    
    * .
    
    * minor fix
    
    * remove chinese chars
    
    * remove key-minitoolkit
    
    * key-toolkit->key-core
    
    * adjust depdendency
    
    * merge key-hd
    
    * springboot key core
    
    * 功能已完善
    
    * better interfaces and tests
    
    * add shell
    
    * .
    
    * change dir
    
    * fix typo
    
    * fix seed generation issues
    
    * fix sharding typos
    
    * fix seed generation issues
    
    * .
    
    * fix name
    
    * renaming
    
    * .
    
    * update docs
    
    * 修复前端界面bug
    
    * make private key and public key self-describing
    
    * add key presenter
    
    * make it has prefix 0x
    
    * .
    
    * add docs
    
    * add javadoc
    
    * add java doc
    
    * make compatible with java sdk
    
    * update encrypt
    
    * make it compatible with small private key(< 32 bytes)
    
    * .
    
    * .

---
+++ key-mini-toolkit/src/main/java/com/webank/wedpr/utils/NativeUtils.java
@@ -0,0 +1,79 @@
+package com.webank.wedpr.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.*;
+
+/**
+ * load native resource
+ * @author aaronchu
+ * @Description
+ * @data 2020/06/18
+ */
+public class NativeUtils {
+
+    public static void loadLibrary(String resourcePath) throws IOException{
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        loadLibrary(resourcePath, classLoader);
+    }
+
+    public static void loadLibrary(String resourcePath, ClassLoader classLoader) throws IOException {
+        File tmpDir = new File(System.getProperty("user.home"));
+        if (!tmpDir.exists() || !tmpDir.isDirectory()) {
+            throw new IOException("user dir unavailable");
+        }
+        File ffiDir = new File(new File(tmpDir, ".fisco"), "nativeutils");
+
+        if (!ffiDir.exists() || !ffiDir.isDirectory()) {
+            if (!ffiDir.mkdirs()) {
+                throw new IOException("failed to create temp folder");
+            }
+        }
+
+        String fileName = deduceFileName(resourcePath);
+        File tmpFile = new File(ffiDir, fileName);
+
+        //To make sure write and load is atomic, incase p1 writes fails p2 load
+        File lockFile = new File(ffiDir, "native.lock");
+        lockFile.deleteOnExit();
+        FileLock lock = null;
+        try (FileChannel c = new FileOutputStream(lockFile, true).getChannel()) {
+            lock = c.lock();
+
+            try (InputStream input = classLoader.getResourceAsStream(resourcePath);) {
+                if (input == null) {
+                    throw new IOException("Resource not found:" + resourcePath + " for classloader " + classLoader.toString());
+                }
+                Files.copy(input, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (AccessDeniedException e) {
+                //In case that p1 load fails p2 write
+            }
+
+            System.load(tmpFile.getAbsolutePath());
+        }
+        finally {
+            if (lock != null) {
+                try{
+                    lock.release();
+                    lockFile.delete();
+                }catch (Exception e){}
+            }
+        }
+    }
+
+    private static String deduceFileName(String path){
+        String[] parts = path.split("/");
+        if(parts.length > 0){
+            return parts[parts.length-1];
+        }
+        throw new IllegalArgumentException("invalid path "+path);
+    }
+
+    private NativeUtils(){}
+}
