722d8c9ed5536b27a78dcb5d5fc180b1fb5085cf
aoronchu <aoronchu@gmail.com>
2021-12-01 14:14:42 +0800

    .

--- key-mini-toolkit/src/main/java/com/webank/wedpr/utils/EnvironmentUtils.java
+++ key-mini-toolkit/src/main/java/com/webank/wedpr/utils/EnvironmentUtils.java
@@ -7,21 +7,27 @@ package com.webank.wedpr.utils;
  */
 public class EnvironmentUtils {
 
-    public static String getResourceTailByOs(String osName){
-        if(osName == null || osName.isEmpty()){
+    public static String getResourceTailByOs(String osName) {
+        if (osName == null || osName.isEmpty()) {
             throw new IllegalArgumentException("osName cannot be null or empty");
         }
         osName = osName.toLowerCase();
-        if(osName.contains("windows")) return ".dll";
-        if(osName.contains("linux")) {
-            String osArch = System.getProperty("os.arch").toLowerCase();
-            if("aarch64".equals(osArch)){
+        String osArch = System.getProperty("os.arch").toLowerCase();
+        System.out.println("osArch:"+osArch);
+        if (osName.contains("windows")) return ".dll";
+        if (osName.contains("linux")) {
+            if ("aarch64".equals(osArch)) {
                 return "_arm.so";
             }
             return ".so";
         }
-        if(osName.contains("mac")) return ".dylib";
-        throw new IllegalArgumentException("does not support os :"+osName);
+        if (osName.contains("mac")) {
+            if ("aarch64".equals(osArch)) {
+                return "_arm.dylib";
+            }
+            return ".dylib";
+        }
+        throw new IllegalArgumentException("does not support os :" + osName);
     }
 
     private EnvironmentUtils(){}
